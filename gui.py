cb28ee24e974b59e6b4663515bc94c7b7883ca35
Marin Veršić <marin.versic101@gmail.com>
2017-02-24 11:47:38 +0100

    Adding production server

--- gui.py
+++ gui.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python2.7
 # coding=utf-8
 
-import json
+from gevent import monkey
+monkey.patch_all()
+
 from flask import Flask, Response, render_template, request, stream_with_context, g
 
+from gevent.pywsgi import WSGIServer
+
+import json
+
 IP_ADDRESS = "0.0.0.0"
-PORT = 5000
+PORT = 5500
 
 app = Flask(__name__)
 app.debug = False
@@ -25,5 +31,8 @@ def parameters():
     return json.dumps(parameters)
 
 if __name__ == "__main__":
-    app.run(debug = True, threaded = True)
+#    app.run(debug = True, threaded = True)
+
+    HTTPServer = WSGIServer((IP_ADDRESS, PORT), app)
+    HTTPServer.serve_forever()
 
